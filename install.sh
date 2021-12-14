35fc79d0d0e20b4e624bf902cbb0cb68aea43e53
Oscar Täckström <oscar@inceptive.team>
2021-12-14 13:08:45 +0100

    Dotfiles init

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Install additional tools
+sudo apt-get install -y zsh
+sudo chsh -s /bin/zsh $USER
+
+# Install and configure Oh My ZSH (if it is not already installed)
+if [ -d "/home/coder/.oh-my-zsh" ] 
+then
+    echo "oh-my-zsh is already installed" 
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    # Append zshrc stuff to end of file
+    touch ~/.zshrc # not sure if this will always exist at this point :/
+    cat .zshrc >> ~/.zshrc
+fi
+
+DOTFILES_CLONE_PATH=$HOME/dotfiles
+for dotfile in "$DOTFILES_CLONE_PATH/".*; do
+  # Skip `..` and '.'
+  [[ $dotfile =~ \.{1,2}$ ]] && continue
+  # Skip Git related
+  [[ $dotfile =~ \.git$ ]] && continue
+  [[ $dotfile =~ \.gitignore$ ]] && continue
+  [[ $dotfile =~ \.gitattributes$ ]] && continue
+
+  echo "Symlinking $dotfile"
+  ln -sf "$dotfile" "$HOME"
+done
+
+# Link VS Code settings
+#ln -sf $DOTFILES_CLONE_PATH/.local/share/code-server/User/settings.json $HOME/.local/share/code-server/User
