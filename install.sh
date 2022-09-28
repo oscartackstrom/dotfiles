51c9840a4ff84308ce74e59ba776972d21f3541f
Oscar Täckström <oscar@inceptive.team>
2022-09-28 12:07:00 +0200

    Install git machete

--- install.sh
+++ install.sh
@@ -21,6 +21,16 @@ else
     curl -s -o $HOME/.oh-my-zsh/custom/themes/common.zsh-theme https://raw.githubusercontent.com/jackharrisonsherlock/common/master/common.zsh-theme
 fi
 
+# Install and configure git-machete (if it is not already installed).
+if [ -d "~/.oh-my-zsh/custom/plugins/git-machete/" ]
+then
+    echo "git-machete is already installed"
+else
+    sudo -H pip install --user -U git-machete
+    mkdir -p ~/.oh-my-zsh/custom/plugins/git-machete/
+    curl -L https://raw.githubusercontent.com/VirtusLab/git-machete/master/completion/git-machete.completion.zsh -o $HOME/.oh-my-zsh/custom/plugins/git-machete/git-machete.plugin.zsh
+fi
+
 DOTFILES_CLONE_PATH=$HOME/dotfiles
 for dotfile in "$DOTFILES_CLONE_PATH/".*; do
   # Skip `..` and '.'
